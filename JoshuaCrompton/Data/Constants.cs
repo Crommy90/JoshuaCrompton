d611155774be644d21a6ee7a44c431835c2b8b37
joshua_crompton <josh.m.crompton@gmail.com>
2022-04-17 21:13:54 +0100

    Added FM and SI pages, added card component

---
+++ JoshuaCrompton/Data/Constants.cs
@@ -0,0 +1,11 @@
+namespace JoshuaCrompton.Data
+{
+    public class Constants
+    {
+        public const string FMPageURL = "/fm";
+        public const string MMPageURL = "/mm";
+
+        public const string ClassCard = "h-100 p-5 bg-dark text-white border rounded-3";
+        public const string ClassJumboTron = "p-5 mb-4 bg-light rounded-3";
+    }
+}
