82ba55890156af7bf82a5bd0255978b19cbcda7f
joshua_crompton <josh.m.crompton@gmail.com>
2022-04-17 22:03:39 +0100

    Started defining some colours

--- JoshuaCrompton/Data/Constants.cs
+++ JoshuaCrompton/Data/Constants.cs
@@ -5,7 +5,7 @@ namespace JoshuaCrompton.Data
         public const string FMPageURL = "/fm";
         public const string MMPageURL = "/mm";
 
-        public const string ClassCard = "h-100 p-5 bg-dark text-white border rounded-3";
-        public const string ClassJumboTron = "p-5 mb-4 bg-light rounded-3";
+        public const string ClassCard = "h-100 p-5 border rounded-3";
+        public const string ClassJumboTron = "p-5 mb-4 rounded-3";
     }
 }
