21d8cab12363064c90b916f9e136d0ed748b0f27
joshua_crompton <josh.m.crompton@gmail.com>
2022-04-18 13:19:50 +0100

    Further work on styling, plus work on fleshing out the index page

--- JoshuaCrompton/Data/Constants.cs
+++ JoshuaCrompton/Data/Constants.cs
@@ -2,10 +2,14 @@ namespace JoshuaCrompton.Data
 {
     public class Constants
     {
-        public const string FMPageURL = "/fm";
+        public const string SIPageURL = "/si";
         public const string MMPageURL = "/mm";
 
         public const string ClassCard = "h-100 p-5 border rounded-3";
         public const string ClassJumboTron = "p-5 mb-4 rounded-3";
+
+        public const string LinkLinkedIn = "https://uk.linkedin.com/in/joshua-crompton";
+        public const string LinkTwitter = "https://twitter.com/Crommy";
     }
+    
 }
