17c5f94fa3b6c81c537b29402099e302d1dfa621
joshua_crompton <josh.m.crompton@gmail.com>
2022-05-01 16:14:29 +0100

    Converted to a single page app

--- JoshuaCrompton/Data/Constants.cs
+++ JoshuaCrompton/Data/Constants.cs
@@ -2,9 +2,6 @@ namespace JoshuaCrompton.Data
 {
     public class Constants
     {
-        public const string SIPageURL = "/si";
-        public const string MMPageURL = "/mm";
-
         public const string ClassCard = "h-100 p-5 border rounded-3";
         public const string ClassJumboTron = "p-5 mb-4 rounded-3";
 
